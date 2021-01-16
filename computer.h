227ab98dc4e508bcfe46a032cfbb8f98aa6ed86d
Tomasz Nitsch <tn418434@students.mimuw.edu.pl>
2021-01-16 20:54:27 +0100

    Beginning / Basic computer structure / computer memory structure / class structure

--- computer.h
+++ computer.h
@@ -1,4 +1,49 @@
 #ifndef OOASM_COMPUTER_H
 #define OOASM_COMPUTER_H
 
+#include <array>
+#include <vector>
+#include "ooasm.h"
+#include "computer_memory.h"
+
+class Computer {
+private:
+    ComputerMemory cm;
+
+    //Executes all declarations of the program.
+    void declare_vars(program &p) {
+        for (auto command : p) {
+            if (command.is_definition()) {
+                command.execute(cm);
+            }
+        }
+    }
+
+    //Executes all functions that aren't declarations.
+    void execute_functions(program &p) {
+        for (auto command : p) {
+            if (!command.is_definition()) {
+                command.execute(cm);
+            }
+        }
+    }
+
+public:
+    explicit Computer(const int &size) {
+        cm.setup(size);
+    }
+
+    void boot(program &p) {
+        declare_vars(p);
+        execute_functions(p);
+    }
+
+    void memory_dump(std::ostream& os) {
+        for (auto a : cm.mem) {
+        //    os << a;
+        }
+    }
+};
+
+
 #endif //OOASM_COMPUTER_H
