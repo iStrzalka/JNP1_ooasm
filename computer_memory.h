dfae1693695d082b5815f65d8d6c088b3342aa73
bk418339 <kozarynabartlomiej@gmail.com>
2021-01-20 18:04:13 +0100

    Add unored_map. add ugly test

--- computer_memory.h
+++ computer_memory.h
@@ -4,6 +4,7 @@
 // Memory of the computer.
 #include <cstdint>
 #include <vector>
+#include <unordered_map>
 #include <stdexcept>
 
 using flag_t = bool;
@@ -12,7 +13,8 @@ using identifier_t = uint64_t;
 
 struct ComputerMemory {
     using memory_t = std::vector<memory_word_t>;
-    using vars_memory_t = std::vector<identifier_t>;
+    //using vars_memory_t = std::vector<identifier_t>;
+    using vars_memory_t = std::unordered_map<identifier_t, identifier_t>;
     using vars_size_t = typename vars_memory_t::size_type;
 
     vars_memory_t vars;
@@ -23,7 +25,7 @@ struct ComputerMemory {
     vars_size_t size = -1;
 
     void setup(const int &_size) {
-        vars = vars_memory_t(_size);
+        vars = vars_memory_t();
         mem = memory_t(_size);
         size = _size;
     }
@@ -34,19 +36,24 @@ struct ComputerMemory {
         if (last_index == size)
             throw std::invalid_argument("Too many variables");
 
-        vars[last_index] = id;
+        //vars[last_index] = id;
+        vars[id] = last_index;
         return last_index++;
     }
 
     // Finds which index of the memory identifier is assigned to and returns it.
     // Throws an error if it can't find it.
     [[nodiscard]] vars_size_t idx(identifier_t id) const {
-        for (vars_size_t i = 0; i < size; i++) {
-            if (vars[i] == id) {
-                return i;
-            }
-        }
-        throw std::invalid_argument("Variable not found");
+//        for (vars_size_t i = 0; i < size; i++) {
+//            if (vars[i] == id) {
+//                return i;
+//            }
+//        }
+        auto it = vars.find(id);
+        if (it != vars.end())
+            return it->second;
+        else
+            throw std::invalid_argument("Variable not found");
     }
 
     // Returns memory at index [index]. Throws an error if it is out of bounds.
--- ooasm_example.cc
+++ ooasm_example.cc
@@ -51,4 +51,51 @@ int main() {
     Computer computer4(5);
     computer4.boot(ooasm_operations);
     assert(memory_dump(computer4) == "6 2 0 0 0 ");
+
+    auto oneki = program({
+        onez(mem(num(18))),
+        ones(mem(num(17))),
+        data("xd1", num(4)),
+        data("xd2", num(3)),
+        data("xd3", num(2)),
+        data("xd4", num(1)),
+        data("xd11", num(4)),
+        data("xd22", num(3)),
+        data("xd33", num(2)),
+        data("xd44", num(1)),
+        data("xd111", num(4)),
+        data("xd222", num(3)),
+        data("xd333", num(2)),
+        data("xd444", num(1)),
+        data("xd1111", num(4)),
+        data("xd2222", num(3)),
+        data("xd3333", num(2)),
+        data("xd4444", num(1)),
+        sub(mem(lea("xd1")), mem(lea("xd11"))),
+        one(mem(num(19))),
+        onez(mem(lea("xd1"))),
+        add(mem(lea("xd2")), mem(num(19))),
+        onez(mem(lea("xd1111"))),
+        ones(mem(lea("xd111"))),
+        dec(mem(lea("xd4"))),
+        onez(mem(lea("xd2"))),
+        ones(mem(lea("xd3"))),
+        inc(mem(lea("xd4"))),
+        onez(mem(lea("xd11"))),
+        ones(mem(lea("xd3"))),
+        dec(mem(lea("xd3"))),
+        onez(mem(lea("xd11"))),
+        ones(mem(lea("xd111"))),
+        dec(mem(num(19))),
+        onez(mem(lea("xd11"))),
+        ones(mem(lea("xd111"))),
+        dec(mem(num(19))),
+        onez(mem(lea("xd111"))),
+        ones(mem(lea("xd1111"))),
+        ones(mem(lea("xd111"))),
+        mov(mem(lea("xd22")), mem(lea("xd22")))
+    });
+    Computer computer5(20);
+    computer5.boot(oneki);
+    assert(memory_dump(computer5) == "1 1 1 1 1 3 2 1 1 3 2 1 1 3 2 1 0 0 0 -1 ");
 }
