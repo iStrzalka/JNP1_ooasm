f4d6a2c540dc22ef592b306635602c103c5ed78b
bk418339 <kozarynabartlomiej@gmail.com>
2021-01-23 17:36:47 +0100

    Add inlines. Add ooasm namespace. Delete redefinition of id_t

--- computer.h
+++ computer.h
@@ -1,31 +1,27 @@
 #ifndef OOASM_COMPUTER_H
 #define OOASM_COMPUTER_H
 
-#include <array>
-#include <vector>
 #include <ostream>
 #include "ooasm.h"
 #include "computer_memory.h"
 
 class Computer {
 private:
-    ComputerMemory cm;
+    ooasm::ComputerMemory cm;
 
-    //Executes all declarations of the program.
+    // Executes all declarations of the program.
     void declare_vars(program &p) {
         for (const auto &command : p) {
-            if (command->is_definition()) {
+            if (command->is_definition())
                 command->execute(cm);
-            }
         }
     }
 
-    //Executes all functions that aren't declarations.
+    // Executes all functions that aren't declarations.
     void execute_functions(program &p) {
         for (const auto &command : p) {
-            if (!command->is_definition()) {
+            if (!command->is_definition())
                 command->execute(cm);
-            }
         }
     }
 
--- ooasm.h
+++ ooasm.h
@@ -5,270 +5,296 @@
 #include <cstdint>
 #include <stdexcept>
 #include <utility>
-#include <memory> // For the shared_ptr<> template
+#include <memory>
 
-using identifier_t = uint64_t;
+namespace ooasm {
+    // Returns identifier for provided [input]. Big and small letters are undifferentiated.
+    identifier_t Id(const char *input) {
+        identifier_t base = 601;
+        identifier_t x = 1;
 
-// Returns identifier for provided [input]. Big and small letters are undifferentiated.
-identifier_t Id(const char *input) {
-    identifier_t base = 601;
-    identifier_t x = 1;
+        if (input == nullptr || input[0] == '\0')
+            throw std::invalid_argument("Input should be not empty nor NULL");
 
-    if (input[0] == '\0')
-        throw std::invalid_argument("Input should be not empty");
-    identifier_t base2 = 1;
-    for (std::size_t i = 0; input[i] != '\0'; i++) {
-        if (i == 10)
-            throw std::invalid_argument("Input's size is over the limit of 6 characters");
+        identifier_t base2 = 1;
+        for (std::size_t i = 0; input[i] != '\0'; i++) {
+            if (i == 10)
+                throw std::invalid_argument("Input's size is over the limit of 10 characters");
 
-        int integer = input[i];
+            uint8_t integer = input[i];
 
-        x += base2 * integer;
-        base2 *= base;
-    }
-
-    const identifier_t result = x;
-    return result;
-}
-
-class RValue {
-    friend class Computer;
-
-public:
-    virtual int get_value([[maybe_unused]] ComputerMemory &mem) const = 0;
-
-    virtual ~RValue() = default;
-};
-
-class LValue { // są pure pure virtual wiec nie potrzebują chyba wirtualnych destruktorów
-    friend class Computer;
-
-public:
-    virtual memory_word_t &get_reference(ComputerMemory &mem) const = 0;
-
-    virtual ~LValue() = default;
-
-    virtual int get_value([[maybe_unused]] ComputerMemory &mem) const = 0;
-};
-
-class Num : public RValue {
-private:
-    int64_t value;
-public:
-    explicit Num(int64_t val) : value(val) {}
-
-    int get_value([[maybe_unused]] ComputerMemory &mem) const override {
-        return value;
-    };
-};
-
-std::shared_ptr<Num> num(int64_t val) {
-    return std::make_shared<Num>(val);
-}
-
-class Lea : public RValue {
-private:
-    identifier_t id;
-public:
-    explicit Lea(const char *_id) : id(Id(_id)) {}
-
-    int get_value(ComputerMemory &mem) const override {
-        return mem.idx(id);
-    };
-};
-
-std::shared_ptr<Lea> lea(const char *_id) {
-    return std::make_shared<Lea>(_id);
-}
-
-class Mem : public LValue, public RValue {
-private:
-    std::shared_ptr<RValue> rval;
-public:
-    explicit Mem(std::shared_ptr<RValue>& x) : rval(std::move(x)) {}
-
-    int get_value(ComputerMemory &mem) const override {
-        return mem.at(rval->get_value(mem));
-    };
-
-    memory_word_t &get_reference(ComputerMemory &mem) const override {
-        return mem.at(rval->get_value(mem));
-    }
-};
-
-std::shared_ptr<Mem> mem(std::shared_ptr<RValue> x) {
-    return std::make_shared<Mem>(x);
-}
-
-class Function {
-protected:
-    bool definition = false;
-public:
-    virtual void execute(ComputerMemory &mem) = 0;
-
-    virtual ~Function() = default;
-
-    [[nodiscard]] bool is_definition() const noexcept {
-        return definition;
-    }
-};
-
-class Data : public Function {
-private:
-    identifier_t data_id;
-    std::shared_ptr<Num> data_num;
-public:
-    Data(const char *input, std::shared_ptr<Num>& _num) : data_id(Id(input)), data_num(_num) {
-        definition = true;
-    }
-
-    void execute(ComputerMemory &mem) override {
-        mem.at(mem.add(data_id)) = data_num->get_value(mem);
-    }
-};
-
-std::shared_ptr<Data> data(const char *input, std::shared_ptr<Num> _num) {
-    return std::make_shared<Data>(input, _num);
-}
-
-class Mov : public Function {
-private:
-    std::shared_ptr<LValue> lval;
-    std::shared_ptr<RValue> rval;
-public:
-    explicit Mov(std::shared_ptr<LValue>& _lval, std::shared_ptr<RValue>& _rval) : lval(_lval),
-                                                                                 rval(_rval) {}
-
-    void execute(ComputerMemory &mem) override {
-        lval->get_reference(mem) = rval->get_value(mem);
-    }
-};
-
-std::shared_ptr<Mov> mov(std::shared_ptr<LValue> _lval, std::shared_ptr<RValue> _rval) {
-    return std::make_shared<Mov>(_lval, _rval);
-}
-
-class Arithmetic : public Function {
-protected:
-    std::shared_ptr<LValue> lval;
-    std::shared_ptr<RValue> rval;
-    bool negate = false;
-
-    explicit Arithmetic(std::shared_ptr<LValue>& _lval, std::shared_ptr<RValue>& _rval) : lval(_lval),
-                                                                                        rval(_rval) {}
-    explicit Arithmetic(std::shared_ptr<LValue>& _lval, std::shared_ptr<RValue>&& _rval) : lval(_lval),
-                                                                                          rval(_rval) {}
-
-    void execute(ComputerMemory &mem) override {
-        auto &lref = lval->get_reference(mem);
-        if (negate) {
-            lref -= rval->get_value(mem);
-        } else {
-            lref += rval->get_value(mem);
+            x += base2 * (integer + 1);
+            base2 *= base;
         }
-        mem.set_flags(lref);
+
+        const identifier_t result = x;
+        return result;
     }
-};
 
-class Add : public Arithmetic {
-public:
-    explicit Add(std::shared_ptr<LValue>& _lval, std::shared_ptr<RValue>& _rval) : Arithmetic(_lval,
-                                                                                            _rval) {}
-};
+    // Designed as virtual class to return the value to what it is pointing to.
+    class RValue {
+    public:
+        virtual memory_word_t get_value([[maybe_unused]] ComputerMemory &mem) const = 0;
 
-std::shared_ptr<Add> add(std::shared_ptr<LValue> _lval, std::shared_ptr<RValue> _rval) {
-    return std::make_shared<Add>(_lval, _rval);
-}
+        virtual ~RValue() = default;
+    };
 
-class Sub : public Arithmetic {
-public:
-    explicit Sub(std::shared_ptr<LValue>& _lval, std::shared_ptr<RValue>& _rval) : Arithmetic(_lval,
-                                                                                            _rval) {
-        negate = true;
-    }
-};
+    // Designed as virtual class to return the reference to what it is pointing to.
+    class LValue {
+    public:
+        virtual memory_word_t &get_reference(ComputerMemory &mem) const = 0;
 
-std::shared_ptr<Sub> sub(std::shared_ptr<LValue> _lval, std::shared_ptr<RValue> _rval) {
-    return std::make_shared<Sub>(_lval, _rval);
-}
+        virtual ~LValue() = default;
+    };
 
-class Inc : public Arithmetic {
-public:
-    explicit Inc(std::shared_ptr<LValue>& _lval) : Arithmetic(_lval, num(1)) {}
-};
+    // Designed as a class that inherits from RValue to return its value.
+    class Num : public RValue {
+    private:
+        memory_word_t value;
+    public:
+        explicit Num(memory_word_t val) : value(val) {}
 
-std::shared_ptr<Inc> inc(std::shared_ptr<LValue> _lval) {
-    return std::make_shared<Inc>(_lval);
-}
+        memory_word_t get_value([[maybe_unused]] ComputerMemory &mem) const override {
+            return value;
+        };
+    };
 
-class Dec : public Arithmetic {
-public:
-    explicit Dec(std::shared_ptr<LValue>& _lval) : Arithmetic(_lval, num(1)) {
-        negate = true;
-    }
-};
+    // Designed as a class that inherits from RValue to return which cell in memory
+    // its Id is pointing to.
+    class Lea : public RValue {
+    private:
+        identifier_t id;
+    public:
+        explicit Lea(const char *_id) : id(Id(_id)) {}
 
-std::shared_ptr<Dec> dec(std::shared_ptr<LValue> _lval) {
-    return std::make_shared<Dec>(_lval);
-}
+        memory_word_t get_value(ComputerMemory &mem) const override {
+            return mem.idx(id);
+        };
+    };
 
-class Flagged : public Function { //Fixme : Better name for that.
-protected:
-    std::shared_ptr<LValue> lval;
+    // Designed as a class that inherits from both RValue and LValue.
+    // On get_value() call returns value of cell in memory based on value of RValue.
+    // On get_reference() call returns reference to the cell in memory based on value of RValue.
+    class Mem : public LValue, public RValue {
+    private:
+        std::shared_ptr<RValue> rval;
+    public:
+        explicit Mem(std::shared_ptr<RValue> &x) : rval(std::move(x)) {}
 
-    explicit Flagged(std::shared_ptr<LValue>& _lval) : lval(_lval) {}
-};
+        memory_word_t get_value(ComputerMemory &mem) const override {
+            return mem.at(rval->get_value(mem));
+        };
 
-class One : public Flagged {
-public:
-    explicit One(std::shared_ptr<LValue>& _lval) : Flagged(_lval) {}
+        memory_word_t &get_reference(ComputerMemory &mem) const override {
+            return mem.at(rval->get_value(mem));
+        }
+    };
 
-    void execute(ComputerMemory &mem) override {
-        lval->get_reference(mem) = 1;
-    }
-};
+    // Designed as a virtual class that can execute it's functionality on given memory.
+    class Function {
+    protected:
+        bool definition = false;
+    public:
+        virtual void execute(ComputerMemory &mem) = 0;
 
-std::shared_ptr<One> one(std::shared_ptr<LValue> _lval) {
-    return std::make_shared<One>(_lval);
-}
+        virtual ~Function() = default;
 
-class Ones : public Flagged {
-public:
-    explicit Ones(std::shared_ptr<LValue>& _lval) : Flagged(_lval) {}
+        [[nodiscard]] bool is_definition() const noexcept {
+            return definition;
+        }
+    };
 
-    void execute(ComputerMemory &mem) override {
-        if (mem.is_flag_SF_set()) {
+    // Designed as a class that inherits from Function that adds new identifier to memory
+    // and assigns value to it.
+    class Data : public Function {
+    private:
+        identifier_t data_id;
+        std::shared_ptr<Num> data_num;
+    public:
+        Data(const char *input, std::shared_ptr<Num> &_num) : data_id(Id(input)), data_num(_num) {
+            definition = true;
+        }
+
+        void execute(ComputerMemory &mem) override {
+            mem.at(mem.add(data_id)) = data_num->get_value(mem);
+        }
+    };
+
+    // Designed as a class that inherits from Function to overwrite memory cell referenced
+    // by LValue by value of RValue.
+    class Mov : public Function {
+    private:
+        std::shared_ptr<LValue> lval;
+        std::shared_ptr<RValue> rval;
+    public:
+        explicit Mov(std::shared_ptr<LValue> &_lval,
+                     std::shared_ptr<RValue> &_rval) : lval(_lval), rval(_rval) {}
+
+        void execute(ComputerMemory &mem) override {
+            lval->get_reference(mem) = rval->get_value(mem);
+        }
+    };
+
+    // Designed as a virtual class that inherits from Function to be responsible
+    // for arithmetic functions of the program.
+    class Arithmetic : public Function {
+    protected:
+        std::shared_ptr<LValue> lval;
+        std::shared_ptr<RValue> rval;
+        bool negate = false;
+
+        explicit Arithmetic(std::shared_ptr<LValue> &_lval,
+                            std::shared_ptr<RValue> &_rval) : lval(_lval), rval(_rval) {}
+
+        explicit Arithmetic(std::shared_ptr<LValue> &_lval,
+                            std::shared_ptr<RValue> &&_rval) : lval(_lval), rval(_rval) {}
+
+        void execute(ComputerMemory &mem) override {
+            auto &lref = lval->get_reference(mem);
+
+            if (negate)
+                lref -= rval->get_value(mem);
+            else
+                lref += rval->get_value(mem);
+
+            mem.set_flags(lref);
+        }
+    };
+
+    // Designed as a class that inherits from Arithmetic to perform addition of value from RValue
+    // to LValue's referenced memory cell and storing it in said cell. Sets flags if needed.
+    class Add : public Arithmetic {
+    public:
+        explicit Add(std::shared_ptr<LValue> &_lval,
+                     std::shared_ptr<RValue> &_rval) : Arithmetic(_lval, _rval) {}
+    };
+
+    // Designed as a class that inherits from Arithmetic to perform subtraction of value from RValue
+    // to LValue's referenced memory cell and storing it in said cell. Sets flags if needed.
+    class Sub : public Arithmetic {
+    public:
+        explicit Sub(std::shared_ptr<LValue> &_lval,
+                     std::shared_ptr<RValue> &_rval) : Arithmetic(_lval, _rval) {
+            negate = true;
+        }
+    };
+
+    // Designed as a class that inherits from Arithmetic to perform incrementing
+    // LValue's referenced memory cell and storing it in said cell. Sets flags if needed.
+    class Inc : public Arithmetic {
+    public:
+        explicit Inc(std::shared_ptr<LValue> &_lval) : Arithmetic(_lval, std::make_shared<ooasm::Num>(1)) {}
+    };
+
+    // Designed as a class that inherits from Arithmetic to perform decrementing
+    // LValue's referenced memory cell and storing it in said cell. Sets flags if needed.
+    class Dec : public Arithmetic {
+    public:
+        explicit Dec(std::shared_ptr<LValue> &_lval) : Arithmetic(_lval, std::make_shared<ooasm::Num>(1)) {
+            negate = true;
+        }
+    };
+
+    // Designed as a virtual class that inherits from Function to be responsible
+    // for assigning ones to memory cell referenced by LValue.
+    class Flagged : public Function {
+    protected:
+        std::shared_ptr<LValue> lval;
+
+        explicit Flagged(std::shared_ptr<LValue> &_lval) : lval(_lval) {}
+    };
+
+    // Designed as a virtual class that inherits from Flagged to be responsible
+    // for assigning one to LValue of the program regardless of the state of both flags.
+    class One : public Flagged {
+    public:
+        explicit One(std::shared_ptr<LValue> &_lval) : Flagged(_lval) {}
+
+        void execute(ComputerMemory &mem) override {
             lval->get_reference(mem) = 1;
         }
-    }
-};
+    };
 
-std::shared_ptr<Ones> ones(std::shared_ptr<LValue> _lval) {
-    return std::make_shared<Ones>(_lval);
+    // Designed as a virtual class that inherits from Flagged to be responsible
+    // for assigning one to LValue of the program only when flag SF is set.
+    class Ones : public Flagged {
+    public:
+        explicit Ones(std::shared_ptr<LValue> &_lval) : Flagged(_lval) {}
+
+        void execute(ComputerMemory &mem) override {
+            if (mem.is_flag_SF_set())
+                lval->get_reference(mem) = 1;
+        }
+    };
+
+    // Designed as a virtual class that inherits from Flagged to be responsible
+    // for assigning one to LValue of the program only when flag ZF is set.
+    class Onez : public Flagged {
+    public:
+        explicit Onez(std::shared_ptr<LValue> &_lval) : Flagged(_lval) {}
+
+        void execute(ComputerMemory &mem) override {
+            if (mem.is_flag_ZF_set())
+                lval->get_reference(mem) = 1;
+        }
+    };
 }
 
-class Onez : public Flagged {
-public:
-    explicit Onez(std::shared_ptr<LValue>& _lval) : Flagged(_lval) {}
+inline std::shared_ptr<ooasm::Num> num(int64_t val) {
+    return std::make_shared<ooasm::Num>(val);
+}
 
-    void execute(ComputerMemory &mem) override {
-        if (mem.is_flag_ZF_set()) {
-            lval->get_reference(mem) = 1;
-        }
-    }
-};
+inline std::shared_ptr<ooasm::Lea> lea(const char *_id) {
+    return std::make_shared<ooasm::Lea>(_id);
+}
 
-std::shared_ptr<Onez> onez(std::shared_ptr<LValue> _lval) {
-    return std::make_shared<Onez>(_lval);
+inline std::shared_ptr<ooasm::Mem> mem(std::shared_ptr<ooasm::RValue> x) {
+    return std::make_shared<ooasm::Mem>(x);
+}
+
+inline std::shared_ptr<ooasm::Data> data(const char *input, std::shared_ptr<ooasm::Num> _num) {
+    return std::make_shared<ooasm::Data>(input, _num);
+}
+
+inline std::shared_ptr<ooasm::Mov> mov(std::shared_ptr<ooasm::LValue> _lval, std::shared_ptr<ooasm::RValue> _rval) {
+    return std::make_shared<ooasm::Mov>(_lval, _rval);
+}
+
+inline std::shared_ptr<ooasm::Add> add(std::shared_ptr<ooasm::LValue> _lval, std::shared_ptr<ooasm::RValue> _rval) {
+    return std::make_shared<ooasm::Add>(_lval, _rval);
+}
+
+inline std::shared_ptr<ooasm::Sub> sub(std::shared_ptr<ooasm::LValue> _lval, std::shared_ptr<ooasm::RValue> _rval) {
+    return std::make_shared<ooasm::Sub>(_lval, _rval);
+}
+
+inline std::shared_ptr<ooasm::Inc> inc(std::shared_ptr<ooasm::LValue> _lval) {
+    return std::make_shared<ooasm::Inc>(_lval);
+}
+
+inline std::shared_ptr<ooasm::Dec> dec(std::shared_ptr<ooasm::LValue> _lval) {
+    return std::make_shared<ooasm::Dec>(_lval);
+}
+
+inline std::shared_ptr<ooasm::One> one(std::shared_ptr<ooasm::LValue> _lval) {
+    return std::make_shared<ooasm::One>(_lval);
+}
+
+inline std::shared_ptr<ooasm::Ones> ones(std::shared_ptr<ooasm::LValue> _lval) {
+    return std::make_shared<ooasm::Ones>(_lval);
+}
+
+inline std::shared_ptr<ooasm::Onez> onez(std::shared_ptr<ooasm::LValue> _lval) {
+    return std::make_shared<ooasm::Onez>(_lval);
 }
 
 class program {
 private:
-    std::vector<std::shared_ptr<Function>> vec;
+    std::vector<std::shared_ptr<ooasm::Function>> vec;
 public:
-    program(std::initializer_list<std::shared_ptr<Function>> init_list) : vec(init_list) {}
+    program(std::initializer_list<std::shared_ptr<ooasm::Function>> init_list) : vec(init_list) {}
 
-    using iterator = typename std::vector<std::shared_ptr<Function>>::iterator;
+    using iterator = typename std::vector<std::shared_ptr<ooasm::Function>>::iterator;
 
     iterator begin() noexcept {
         return vec.begin();
